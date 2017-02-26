b64da0c2e75c29b347df10409d38bfa25440e43f
Nhu Finney <nfinney@dailyinsightgroup.com>
2017-02-25 21:35:45 -0800

    learn loader and query in webpack.

--- 02_03/start/webpack.config.js
+++ 02_03/start/webpack.config.js
@@ -4,15 +4,18 @@ module.exports = {
 		path: 'build',
 		filename: 'bundle.js'
 	},
-    module: {
-        loaders: [ 
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
 };
 //Run webpack in terminal, move to inside the start directory
 //Run $ ./node_modules/.bin/webpack
